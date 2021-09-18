8f43ea39cec84794d031f6bc76130c0d839333ab
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-09-17 21:57:01 -0500

    modified code to include a photo album page

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+
+const useStyles =makeStyles((theme)=>({}));
+export default function () {
+    const classes = useStyles();
+    return(
+        <div className={}>
+            <h1>dbull_ATX</h1>
+        </div>
+    )
+}
