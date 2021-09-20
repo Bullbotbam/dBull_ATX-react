b7ed7b46e237b22ea361abd72839754d39dd3c2d
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-09-20 01:44:43 -0500

    added ref link to social medial

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,28 +3,46 @@ import { Twitter } from '@material-ui/icons';
 import { Instagram } from '@material-ui/icons';
 import { LinkedIn } from '@material-ui/icons';
 import { GitHub } from '@material-ui/icons';
-import MailIcon from '@material-ui/icons/Mail';
+import { EmailOutlinedIcon } from '@material-ui/icons';
 import { IconButton } from '@material-ui/core';
 
 const Sidebar = () => {
 	return (
 		<div className="sidebar">
 			<div className="sidebarDown">
-				<IconButton className="icon-btn">
-					<Twitter fontSize="medium" />
+				<IconButton
+					className="icon-btn"
+					aria-label="Twitter.com"
+					onClick={() => window.open('https://twitter.com/home?lang=en')}
+				>
+					<Twitter fontSize="large" />
 				</IconButton>
-				<IconButton className="icon-btn">
-					<Instagram fontSize="medium" />
+				<IconButton
+					className="icon-btn"
+					aria-label="Instagram.com"
+					onClick={() => window.open('https://www.instagram.com/d.bull_atx/')}
+				>
+					<Instagram fontSize="large" />
 				</IconButton>
-				<IconButton className="icon-btn">
-					<LinkedIn fontSize="medium" />
+				<IconButton
+					className="icon-btn"
+					aria-label="LinkedIn.com"
+					onClick={() =>
+						window.open('https://www.linkedin.com/in/donaldjbull-atx/')
+					}
+				>
+					<LinkedIn fontSize="large" />
 				</IconButton>
-				<IconButton className="icon-btn">
-					<GitHub fontSize="medium" />
+				<IconButton
+					className="icon-btn"
+					aria-label="GitHub.com"
+					onClick={() => window.open('https://github.com/Bullbotbam')}
+				>
+					<GitHub fontSize="large" />
 				</IconButton>
-				<IconButton className="icon-btn">
-					<MailIcon fontSize="medium" />
-				</IconButton>
+				{/* <IconButton className="icon-btn">
+					<EmailOutlinedIcon fontSize="large" />
+				</IconButton> */}
 			</div>
 		</div>
 	);
