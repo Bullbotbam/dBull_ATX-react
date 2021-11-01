a715ea2c22660c0567a71de73d9ee3817a09c223
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-11-01 18:22:39 -0500

    Updates

---
+++ precache-manifest.915a9b08db8f1ff0ceb1d3aca94cd655.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e0fc995764d677183a55a2e652fd4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90deb05de911649a9753",
+    "url": "/static/css/main.d2e1d60e.chunk.css"
+  },
+  {
+    "revision": "3b4774e96e128d14caf1",
+    "url": "/static/js/2.4c81c267.chunk.js"
+  },
+  {
+    "revision": "a93d0b66c07b20b71f4e7f48926ea830",
+    "url": "/static/js/2.4c81c267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90deb05de911649a9753",
+    "url": "/static/js/main.d00266ee.chunk.js"
+  },
+  {
+    "revision": "91e069d2900290085c8f",
+    "url": "/static/js/runtime-main.a7be0ca2.js"
+  },
+  {
+    "revision": "512ff91740b145a44508689327647e1a",
+    "url": "/static/media/budget_tracker.512ff917.gif"
+  },
+  {
+    "revision": "bf07f0e18cca0a96fd370c7428eae915",
+    "url": "/static/media/dbull_atx_logo.bf07f0e1.png"
+  },
+  {
+    "revision": "56a7147d63c2007a3878a8f1ca1d828e",
+    "url": "/static/media/dbull_background.56a7147d.jpeg"
+  },
+  {
+    "revision": "e80a918a48e5f8c44f507a10cee128de",
+    "url": "/static/media/deep-thoughts.e80a918a.png"
+  },
+  {
+    "revision": "543173e8b4978d478dc5a59173dc82fe",
+    "url": "/static/media/dinner_n_movie.543173e8.png"
+  },
+  {
+    "revision": "66a1b2a25887f46db8ed58b8485b484b",
+    "url": "/static/media/farmgo-logo.66a1b2a2.jpeg"
+  },
+  {
+    "revision": "cd07622473113e884646a215e0cf03a5",
+    "url": "/static/media/highfive.cd076224.PNG"
+  },
+  {
+    "revision": "88bb7f3e226385df459c8053d2a2a25c",
+    "url": "/static/media/know-more.88bb7f3e.gif"
+  }
+]);
