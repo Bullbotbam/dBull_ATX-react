6feb74b4d74c13cc4c1a4303133547389f798f23
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-11-15 15:06:20 -0600

    forcing gh pages

---
+++ build/precache-manifest.b2b7015e448cb0e386bd3e41a96a87b8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2143442716dce5a20a9aa6f4c7fdbe2",
+    "url": "/dbull_atx-react/index.html"
+  },
+  {
+    "revision": "a83ea5b697d0cde5a87e",
+    "url": "/dbull_atx-react/static/css/main.aedc80bd.chunk.css"
+  },
+  {
+    "revision": "bcccad2851d3081321df",
+    "url": "/dbull_atx-react/static/js/2.96b4afb2.chunk.js"
+  },
+  {
+    "revision": "a93d0b66c07b20b71f4e7f48926ea830",
+    "url": "/dbull_atx-react/static/js/2.96b4afb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a83ea5b697d0cde5a87e",
+    "url": "/dbull_atx-react/static/js/main.08e7e214.chunk.js"
+  },
+  {
+    "revision": "2de491614bff76124ab2",
+    "url": "/dbull_atx-react/static/js/runtime-main.003f65c9.js"
+  },
+  {
+    "revision": "512ff91740b145a44508689327647e1a",
+    "url": "/dbull_atx-react/static/media/budget_tracker.512ff917.gif"
+  },
+  {
+    "revision": "bf07f0e18cca0a96fd370c7428eae915",
+    "url": "/dbull_atx-react/static/media/dbull_atx_logo.bf07f0e1.png"
+  },
+  {
+    "revision": "56a7147d63c2007a3878a8f1ca1d828e",
+    "url": "/dbull_atx-react/static/media/dbull_background.56a7147d.jpeg"
+  },
+  {
+    "revision": "e80a918a48e5f8c44f507a10cee128de",
+    "url": "/dbull_atx-react/static/media/deep-thoughts.e80a918a.png"
+  },
+  {
+    "revision": "543173e8b4978d478dc5a59173dc82fe",
+    "url": "/dbull_atx-react/static/media/dinner_n_movie.543173e8.png"
+  },
+  {
+    "revision": "66a1b2a25887f46db8ed58b8485b484b",
+    "url": "/dbull_atx-react/static/media/farmgo-logo.66a1b2a2.jpeg"
+  },
+  {
+    "revision": "cd07622473113e884646a215e0cf03a5",
+    "url": "/dbull_atx-react/static/media/highfive.cd076224.PNG"
+  },
+  {
+    "revision": "88bb7f3e226385df459c8053d2a2a25c",
+    "url": "/dbull_atx-react/static/media/know-more.88bb7f3e.gif"
+  }
+]);
